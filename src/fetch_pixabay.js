8955b9c5307d83b7b95349f79a776ffe184b805e
Timofey_Slakva <111429897+Lefirmor@users.noreply.github.com>
2023-08-20 17:51:29 +0200

    http

--- src/fetch_pixabay.js
+++ src/fetch_pixabay.js
@@ -1,4 +1,28 @@
-// export default function findPicture()
+export default fetchPictures;
 // // const BASE_URL = `https://pixabay.com/api/q=cat`
 // // const API_KEY = '33922902-bfae212e6daaaa40ab37292b2';
 
+function fetchPictures(event) {
+//   event.preventDefault() 
+const URL = 'https://pixabay.com/api/'; // С большой буквы потому что знаем что в переменной наперед
+const API_KEY = '33922902-bfae212e6daaaa40ab37292b2';
+
+const params = new URLSearchParams({
+    api_key: API_KEY,
+    page: 1,
+    q: searchQuery,
+    image_type: photo,
+    orientation: horizontal,
+    safesearch: true
+  });
+
+  return fetch('${URL}?${params}').then(res => {
+    if(!res.ok){
+        throw new Error(res.statusText)
+    }
+    return res.json()
+  });
+
+}
+fetchPictures("cat").then((res) => console.log(res))
+.catch((err) => console.error(err))
--- src/index.js
+++ src/index.js
@@ -1,26 +1,61 @@
-import findPicture from "./fetch_pixabay";
-
 const refs = {
   searchForm: document.querySelector('.search-form'),
+  galleryList: document.querySelector('.gallery-list'),
+  container: document.querySelector('.container'),
+  gallery: document.querySelector('.gallery'),
 
+};
+
+function fetchPictures() {
+  const searchQuery = '';
+
+  return fetch(
+    `https://pixabay.com/api/?key=33922902-bfae212e6daaaa40ab37292b2&q=${'cat'}&image_type=photo&orientation=horizontal&safesearch=true`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
 }
-refs.searchForm.addEventListener('submit', onSearch)
+// refs.searchForm.addEventListener('submit', findpics);
 
-function onSearch(event){
-  event.preventDefault()
+fetchPictures()
+  .then(data => {
+    refs.galleryList.insertAdjacentHTML("beforeend", createMarkup(data.hits))
 
-  const searchQuery = event.currentTarget.elements.query.value;
+    console.log(data)
+  })
+  .catch(err => console.error(err));
 
-  fetch(`https://pixabay.com/api/?key=33922902-bfae212e6daaaa40ab37292b2&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`)
-.then(res => res.json())
-.then(console.log)
+function createMarkup(array) {
+  return array
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+      <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>${likes}</b>
+        </p>
+        <p class="info-item">
+          <b> likes ${views}</b>
+        </p>
+        <p class="info-item">
+          <b>${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>${downloads}</b>
+        </p>
+      </div>
+    </div>`
+    )
+    .join('');
 }
-
-
-
-// const options = {
-//   headers:{
-//     key: "33922902-bfae212e6daaaa40ab37292b2"
-//   },
-// }
-
