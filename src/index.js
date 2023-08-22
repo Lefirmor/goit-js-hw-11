c86f2363726a86a2e01128c0fc849392548ac6e1
Timofey_Slakva <111429897+Lefirmor@users.noreply.github.com>
2023-08-22 20:19:20 +0200

    summary

--- src/index.js
+++ src/index.js
@@ -1,32 +1,56 @@
 const refs = {
+  searchInput: document.querySelector('.search-input'),
   searchForm: document.querySelector('.search-form'),
   galleryList: document.querySelector('.gallery-list'),
   container: document.querySelector('.container'),
   gallery: document.querySelector('.gallery'),
-
+  loadMore: document.querySelector('.load-more')
 };
+
+refs.loadMore.addEventListener("click", OnLoadMore)
+let page = 1;
+function OnLoadMore(){
+  page += 1;
+
+  fetchPictures().then(data => {
+    refs.gallery.insertAdjacentHTML("beforeend", createMarkup(data.hits))
+
+    if(data.hits < data.totalHits){
+      refs.loadMore.classList.replace("load-more", "load-more__hidden")
+    }
+})}
 
 function fetchPictures() {
   const searchQuery = "";
 
   return fetch(
-    `https://pixabay.com/api/?key=33922902-bfae212e6daaaa40ab37292b2&q=${'cat'}&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
-  });
-}
-// refs.searchForm.addEventListener('submit', findpics);
+    `https://pixabay.com/api/?key=33922902-bfae212e6daaaa40ab37292b2&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}`
+  ).then(res >= res.json).then(console.log)
+   
 
-fetchPictures()
-  .then(data => {
-    refs.galleryList.insertAdjacentHTML("beforeend", createMarkup(data.hits))
+}
+refs.searchForm.addEventListener('submit', findpics);
+
+function findpics(e){
+  e.preventDefault()
+  const inputValue = refs.searchForm.elements.searchQuery
+
+  fetchPictures(inputValue.value).then(data => {
+    refs.gallery.insertAdjacentHTML("beforeend", createMarkup(data.hits))
 
     console.log(data)
-  })
-  .catch(err => console.error(err));
+  }).catch(err => console.log(err.statusText))
+    
+  
+}
+
+// fetchPictures()
+//   .then(data => {
+//     refs.gallery.insertAdjacentHTML("beforeend", createMarkup(data.hits))
+
+//     console.log(data)
+//   })
+//   .catch(err => console.error(err));
 
 function createMarkup(array) {
   return array
@@ -40,19 +64,19 @@ function createMarkup(array) {
         comments,
         downloads,
       }) => `<div class="photo-card">
-      <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
       <div class="info">
         <p class="info-item">
-          <b>${likes}</b>
+          <b> likes ${likes}</b>
         </p>
         <p class="info-item">
-          <b> likes ${views}</b>
+          <b> views ${views}</b>
         </p>
         <p class="info-item">
-          <b>${comments}</b>
+          <b> comments ${comments}</b>
         </p>
         <p class="info-item">
-          <b>${downloads}</b>
+          <b> downloads ${downloads}</b>
         </p>
       </div>
     </div>`
