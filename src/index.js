b21b8c1158e3ceb7a6ef9db76dbc35a2cfe41c0a
Timofey_Slakva <111429897+Lefirmor@users.noreply.github.com>
2023-08-26 15:25:57 +0200

    pixabayService

--- src/index.js
+++ src/index.js
@@ -1,58 +1,50 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
+import PixabayApiService from './pixabay-service';
+
 const refs = {
   searchInput: document.querySelector('.search-input'),
   searchForm: document.querySelector('.search-form'),
   galleryList: document.querySelector('.gallery-list'),
   container: document.querySelector('.container'),
   gallery: document.querySelector('.gallery'),
-  loadMore: document.querySelector('.load-more')
+  // loadMoreHidden: document.querySelector('load-more__is-hidden'),
+  loadMore: document.querySelector('.load-more'),
 };
-
-refs.loadMore.addEventListener("click", OnLoadMore)
+const pixabayApiService = new PixabayApiService();
+// refs.loadMore.addEventListener("click", OnLoadMore)
+// const inputValue = refs.searchInput.value;
 let page = 1;
-function OnLoadMore(){
-  page += 1;
 
-  fetchPictures().then(data => {
-    refs.gallery.insertAdjacentHTML("beforeend", createMarkup(data.hits))
 
-    if(data.hits < data.totalHits){
-      refs.loadMore.classList.replace("load-more", "load-more__hidden")
-    }
-})}
-
-function fetchPictures() {
-  const searchQuery = '';
-
-  return fetch(
-    `https://pixabay.com/api/?key=33922902-bfae212e6daaaa40ab37292b2&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}`
-  ).then(res => {
-    return res.json()
-  })
-   
-
-}
 refs.searchForm.addEventListener('submit', findpics);
 
-function findpics(e){
-  e.preventDefault()
-  const inputValue = refs.searchForm.elements.searchQuery
+function findpics(e) {
+  e.preventDefault();
 
-  fetchPictures(inputValue.value).then(data => {
-    refs.gallery.insertAdjacentHTML("beforeend", createMarkup(data.hits))
-
-    console.log(data)
-  }).catch(err => console.log(err.statusText))
-    
   
+  pixabayApiService.query = refs.searchInput.value;
+  pixabayApiService.resetPage()
+  pixabayApiService.fetchPictures()
+    .then(data => {
+      clearArticlesContainer();
+      document.documentElement.getBoundingClientRect().top = 0;
+      if (data.hits.length === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+        refs.loadMore.classList.add('load-more__is-hidden');
+     
+      } else if (data.total !== 0) {
+        Notiflix.Notify.success(`Hooray! We found ${data.total} images`);
+      }
+
+      refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+      lightbox.refresh()
+
+      console.log(data);
+    })
+    .catch(err => console.log(err.statusText));
 }
-
-fetchPictures()
-  .then(data => {
-    refs.gallery.insertAdjacentHTML("beforeend", createMarkup(data.hits))
-
-    console.log(data)
-  })
-  .catch(err => console.error(err));
 
 function createMarkup(array) {
   return array
@@ -66,7 +58,7 @@ function createMarkup(array) {
         comments,
         downloads,
       }) => `<div class="photo-card">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+      <a href = "${webformatURL}"><img src="${largeImageURL}" alt="${tags}" loading="lazy" /></a>
       <div class="info">
         <p class="info-item">
           <b> likes ${likes}</b>
@@ -84,4 +76,30 @@ function createMarkup(array) {
     </div>`
     )
     .join('');
+    
 }
+
+function clearArticlesContainer() {
+  refs.gallery.innerHTML = '';
+}
+console.log(page);
+window.addEventListener('scroll', () => {
+  const documentRect = document.documentElement.getBoundingClientRect();
+  if (documentRect.bottom < document.documentElement.clientHeight + 150) {
+    pixabayApiService.incrementPage()
+    pixabayApiService.query = refs.searchInput.value;
+    pixabayApiService.fetchPictures().then(data => {
+      refs.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+      lightbox.refresh()
+      // console.log(data);
+    });
+  }
+});
+ 
+const lightbox = new SimpleLightbox('.gallery a', {
+ 
+  
+})
+
+
+
---
+++ src/pixabay-service.js
@@ -0,0 +1,32 @@
+export default class PixabayApiService {
+  constructor() {
+    this.searchQuery = ''
+    this.page = 1;
+  }
+
+  async fetchPictures() {
+   return fetch(
+      `https://pixabay.com/api/?key=33922902-bfae212e6daaaa40ab37292b2&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}`
+    ).then(res => res.json())
+    .then(data => {
+      this.incrementPage()
+      return data;
+    })
+  }
+
+  incrementPage(){
+    this.page += 1;
+  }
+
+  resetPage(){
+    this.page = 1;
+  }
+
+  get query(){
+    return this.searchQuery
+  }
+
+  set query(newQuery){
+    this.searchQuery = newQuery;
+  }
+}
